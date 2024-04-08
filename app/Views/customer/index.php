3652604d1385bfe199fa6f1b410782e21c002a3b
Elias Segura <eliasjsegura00@gmail.com>
2024-04-07 20:44:33 -0600

    added Customer form

--- app/Views/customer/index.php
+++ app/Views/customer/index.php
@@ -75,52 +75,43 @@
                 <form>
                     <input type="hidden" value="0" id="txtId">
                     <div class="row">
-                        <div class="col-sm-8">
-                            <div class="form-row">
-                                <div class="form-group col-sm-6">
-                                    <label for="txtNombre">Nombre</label>
-                                    <input type="text" class="form-control form-control-sm input-validar" id="txtNombre" name="Nombre">
-                                </div>
-                                <div class="form-group col-sm-6">
-                                    <label for="txtCorreo">Correo</label>
-                                    <input type="email" class="form-control form-control-sm input-validar" id="txtCorreo" name="Correo">
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="form-group col-sm-6">
-                                    <label for="txtTelefono">Telefono</label>
-                                    <input type="text" class="form-control form-control-sm input-validar" id="txtTelefono" name="Telefono">
-                                </div>
-                                <div class="form-group col-sm-6">
-                                    <label for="cboRol">Rol</label>
-                                    <select class="form-control form-control-sm" id="cboRol">
-                                        <option value="1">Administrador</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="form-group col-sm-6">
-                                    <label for="cboEstado">Estado</label>
-                                    <select class="form-control form-control-sm" id="cboEstado">
-                                        <option value="1">Activo</option>
-                                        <option value="0">No Activo</option>
-                                    </select>
-                                </div>
-                                <div class="form-group col-sm-6">
-                                    <label for="txtFoto">Foto</label>
-                                    <input class="form-control-file" type="file" id="txtFoto" />
-                                </div>
-                            </div>
+                        <div class="form-group col-sm-6">
+                            <label for="txtNombre">Nombre completo</label>
+                            <input type="text" class="form-control form-control-sm input-validar" id="txtNombre" name="name">
                         </div>
-                        <div class="col-sm-4">
-                            <img id="imgUsuario" style="max-width:200px;" src="https://images.unsplash.com/photo-1519648023493-d82b5f8d7b8a?w=300" class="rounded mx-auto d-block" alt="Foto usuario">
+                        <div class="form-group col-sm-6">
+                            <label for="txtCorreo">Correo</label>
+                            <input type="email" class="form-control form-control-sm input-validar" id="txtCorreo" name="email">
                         </div>
-
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-sm-4">
+                            <label for="txtTelefono">Telefono</label>
+                            <input type="text" class="form-control form-control-sm input-validar" id="txtTelefono" name="cell_phone">
+                        </div>
+                        <div class="form-group col-sm-4">
+                            <label for="txtDepartment">Departamento</label>
+                            <input type="text" class="form-control form-control-sm input-validar" id="txtDepartment" name="department">
+                        </div>
+                        <div class="form-group col-sm-4">
+                            <label for="txtMunicipality">Municipio</label>
+                            <input type="text" class="form-control form-control-sm input-validar" id="txtMunicipality" name="municipality">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-sm-4">
+                            <label for="txtDUI">DUI</label>
+                            <input type="text" class="form-control form-control-sm input-validar" id="txtDUI" name="dui">
+                        </div>
+                        <div class="form-group col-sm-8">
+                            <label for="txtAddress">Direccion</label>
+                            <textarea name="address" id="txtAddress" class="form-control form-control-sm input-validar" style="width: 100%; height: 100px; resize: none;"></textarea>
+                        </div>
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
-                <button class="btn btn-primary btn-sm" type="button" id="btnGuardar">Guardar</button>
+                <button class="btn btn-primary btn-sm" type="submit" id="btnSave">Guardar</button>
                 <button class="btn btn-danger btn-sm" type="button" data-dismiss="modal">Cancel</button>
             </div>
         </div>
