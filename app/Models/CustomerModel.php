b291c3ca32883678097717e62891168e243244ad
Elias Segura <eliasjsegura00@gmail.com>
2024-04-13 21:31:10 -0600

    agregue municipalities model

--- app/Models/CustomerModel.php
+++ app/Models/CustomerModel.php
@@ -12,7 +12,7 @@ class CustomerModel extends Model
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
     protected $allowedFields = [
-        'address', 'cell_phone', 'department', 'email', 'id_type_customer', 'municipality', 'name_customer',
-        'no_register_nrc', 'number_dui', 'number_nit', 'social_reason', 'spin'
+        'address', 'cell_phone', 'email', 'id_type_customer', 'name_customer',
+        'no_register_nrc', 'number_dui', 'number_nit', 'social_reason', 'spin', 'id_department', 'id_municipality'
     ];
 }
